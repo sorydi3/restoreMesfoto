182ee0428516df7b6fdabe24fb1f6d3285c5c529
ibradi3 <ibrahimasory22@hotmail.com>
2023-02-20 23:54:50 +0100

    loging page added

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,23 +2,24 @@ import React from 'react'
 import { getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 import app from "./api/firebase";
 import { useState } from "react";
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import redis from './../utils/redis';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import redis from '../utils/redis';
 import { useRouter} from 'next/router';
+import Link from "next/link"
 const auth = getAuth(app);
 
 
 type Props = {}
 //get loggin detaills
-function Logindetaill(props: any) {
+function Login(props: any) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [
-        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
         user,
         loading,
         error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useSignInWithEmailAndPassword(auth);
     const Router = useRouter();
     if (error) {
         return (
@@ -31,32 +32,43 @@ function Logindetaill(props: any) {
         return <p>Loading...</p>;
     }
     if (user) {
-        Router.push('/index');
+        Router.push('/');
     }
     return (
-        <div className="App mt-3 flex flex-col w-80 mx-auto space-y-3 items-center justify-center">
-            <input
-                type="email"
-                className='bg-red-100 h-8 rounded-md px-2'
-                placeholder='Email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                type="password"
-                className='bg-red-100 h-8 rounded-md px-2'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={() => createUserWithEmailAndPassword(email, password)}>
-                Register
-            </button>
+        <div className='w-screen flex h-screen justify-center items-center'>
+            <div className="App flex flex-col w-80 space-y-3 mx-auto  ">
+                <h1>Login</h1>
+                <input
+                    type="email"
+                    className='bg-red-100 h-8 rounded-md px-2'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    className='bg-red-100 h-8 rounded-md px-2'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={() => signInWithEmailAndPassword(email, password)}
+                    className='bg-blue-500 text-white rounded-md px-2 hover:bg-blue-200 hover:text-black'
+                >
+                    Submit
+                </button>
+                <Link
+                    href="\signup"
+                    className='text-blue-500'
+                >
+                    Don't have an account? Sign up
+                </Link>
+            </div>
         </div>
     );
 };
 
-export default Logindetaill;
+export default Login;
 
 /*
 const Login = (props: Props) => {
