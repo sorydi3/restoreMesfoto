4b3da50cf4a3415c505335015dd86658cd0d6c09
ibradi3 <ibrahimasory22@hotmail.com>
2023-02-21 23:55:03 +0100

    redirect users if they are not logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -6,8 +7,23 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SquigglyLines from "../components/SquigglyLines";
 import { Testimonials } from "../components/Testimonials";
+import app from "./api/firebase";
+import {useIdToken } from 'react-firebase-hooks/auth';
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+const auth = getAuth(app);
+import Router from "next/router";
+
+const redirecrToLogin = async () => {
+  typeof window !== "undefined" && Router.push("/login");
+}
 
 const Home: NextPage = () => {
+
+  const [user, loading, error] = useIdToken(auth);
+  // redirect if user if not logged in
+  if(!user) redirecrToLogin();
+
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
@@ -78,3 +94,14 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+// prevent user to view this page uless it's logged in using server side propts
+
+export async function getServerSideProps(props:any) {
+
+  return {
+    props: {},
+  };
+}
+
+
